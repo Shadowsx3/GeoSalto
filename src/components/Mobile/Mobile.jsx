9e2bf7ff7039de16e093d02d24881dd19e5f5b59
Manuel Buslon <41681538+Shadowsx3@users.noreply.github.com>
2021-10-08 00:22:23 -0300

    me rindo

--- src/components/Mobile/Mobile.jsx
+++ src/components/Mobile/Mobile.jsx
@@ -6,22 +6,7 @@ import "./Mobile.scss";
 const Mobile = (props) => {
   return (
     <div className="home">
-      <AppBar
-        position="absolute"
-        sx={{
-          backgroundColor: "#e9f5db",
-          color: "black",
-          border: "solid black",
-        }}
-      >
-        <Divider variant="middle" style={{ marginBottom: "10" }} />
-        <Typography variant="h6" component="h6">
-          Localidades Artesanales Salto
-        </Typography>
-        <Divider variant="middle" style={{ marginBottom: "10" }} />
-      </AppBar>
-      <Paper sx={{ height: "5%", width: "100%", background: "#cfe1b9" }} />
-      <Paper sx={{ height: "95%", width: "100%", background: "#cfe1b9" }}>
+      <Paper sx={{ height: "100%", width: "100%", background: "#cfe1b9" }}>
         <Mapa
           checked={props.checked}
           placesRecords={props.placesRecords}
